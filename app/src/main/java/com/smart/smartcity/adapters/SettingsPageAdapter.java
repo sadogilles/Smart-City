98e3aa6ff7e7575997f307e235bff6c2b60e6ef3
sadogilles <sadowizzy@gmail.com>
2020-05-05 22:25:19 +0200

    Modified Forgot!

--- app/src/main/java/com/smart/smartcity/adapters/SettingsPageAdapter.java
+++ app/src/main/java/com/smart/smartcity/adapters/SettingsPageAdapter.java
@@ -1,18 +1,12 @@
 package com.smart.smartcity.adapters;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.PagerAdapter;
 
-import com.smart.smartcity.AlarmSettingsFragment;
-import com.smart.smartcity.R;
+import com.smart.smartcity.fragment.AlarmSettingsFragment;
 import com.smart.smartcity.fragment.InterestSettingsFragment;
 import com.smart.smartcity.fragment.ProfileSettingsFragment;
 import com.smart.smartcity.fragment.ServiceSettingsFragment;
--- app/src/main/java/com/smart/smartcity/fragment/AlarmSettingsFragment.java
+++ app/src/main/java/com/smart/smartcity/fragment/AlarmSettingsFragment.java
@@ -1,4 +1,4 @@
-package com.smart.smartcity;
+package com.smart.smartcity.fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.smart.smartcity.R;
 
 
 /**
