e076a096fe7064f259197e1c77de977c4d3966c2
sadogilles <sadowizzy@gmail.com>
2020-05-06 18:10:26 +0200

    Layout for Network Creation Done!

--- app/src/main/java/com/smart/smartcity/fragment/CreateNetworkSettingsFragment.java
+++ app/src/main/java/com/smart/smartcity/fragment/CreateNetworkSettingsFragment.java
@@ -1,22 +1,38 @@
 package com.smart.smartcity.fragment;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.smart.smartcity.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageActivity;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CreateNetworkSettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CreateNetworkSettingsFragment extends Fragment {
+public class CreateNetworkSettingsFragment extends Fragment implements View.OnClickListener {
 
+    private static final int PICK_IMAGE=1;
+    private Uri imageURI=null;
+    private ImageView network_image =null;
 
     public CreateNetworkSettingsFragment() {
         // Required empty public constructor
@@ -38,7 +54,60 @@ public class CreateNetworkSettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_network_settings, container, false);
+
+       View v = inflater.inflate(R.layout.fragment_create_network_settings, container, false);
+       network_image= v.findViewById(R.id.upload_network_image);
+
+       network_image.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getContext(),"Upload click",Toast.LENGTH_SHORT).show();
+
+               Intent gallery = new Intent();
+
+               gallery.setType("image/*");
+               gallery.setAction(Intent.ACTION_GET_CONTENT);
+
+               startActivityForResult(Intent.createChooser(gallery,"Select Picture"),PICK_IMAGE);
+           }
+       });
+
+
+       return v;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+
+            imageURI = data.getData();
+
+            CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).setAspectRatio(1, 1).start(getContext(),this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (resultCode == RESULT_OK) {
+
+                Uri resultUri = result.getUri();
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageURI);
+                    network_image.setImageBitmap(bitmap);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
