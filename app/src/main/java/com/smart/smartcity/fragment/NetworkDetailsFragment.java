60cf961ca99dabbb00db82378ca487796f699f48
Basil Dalié <dalie.basil@gmail.com>
2020-05-08 17:26:27 +0200

    Implemented filling of network metadatas ...
    Implemented filling of network metadatas in network details view

--- app/src/main/java/com/smart/smartcity/fragment/NetworkDetailsFragment.java
+++ app/src/main/java/com/smart/smartcity/fragment/NetworkDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.smart.smartcity.fragment;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.google.android.material.textview.MaterialTextView;
 import com.smart.smartcity.R;
+import com.smart.smartcity.context.IDownloadImageContext;
 import com.smart.smartcity.model.Network;
+import com.smart.smartcity.util.DownloadImageTask;
 
 
 /**
@@ -18,9 +24,14 @@ import com.smart.smartcity.model.Network;
  * Use the {@link NetworkDetailsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NetworkDetailsFragment extends Fragment {
+public class NetworkDetailsFragment extends Fragment implements View.OnClickListener, IDownloadImageContext {
     private static final String NETWORK_KEY = "NETWORK";
     private Network network;
+    private MaterialTextView networkName;
+    private MaterialTextView networkDescription;
+    private ImageView networkImage;
+    private Button togglePublishFormButton;
+    private LinearLayout publicationForm;
 
     public NetworkDetailsFragment() {
         // Required empty public constructor
@@ -48,12 +59,41 @@ public class NetworkDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.network_details_fragment, container, false);
+        View view = inflater.inflate(R.layout.network_details_fragment, container, false);
 
         network = (Network) getArguments().getParcelable(NETWORK_KEY);
 
+        networkName = view.findViewById(R.id.network_details_name);
+        networkDescription = view.findViewById(R.id.network_details_description);
+        networkImage = view.findViewById(R.id.network_details_image);
+        togglePublishFormButton = view.findViewById(R.id.toggle_publish_form);
+        publicationForm = view.findViewById(R.id.publication_form);
 
+        networkName.setText(network.getName());
+        networkDescription.setText(network.getDescription());
 
-        return v;
+        new DownloadImageTask(this, network.getId()).execute(network.getImageUrl());
+
+        togglePublishFormButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.toggle_publish_form) {
+            if (publicationForm.getVisibility() == View.GONE) {
+                publicationForm.setVisibility(View.VISIBLE);
+            } else if (publicationForm.getVisibility() == View.VISIBLE) {
+                publicationForm.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @Override
+    public void onImageDownloaded(Bitmap bitmap, int id) {
+        network.setImageBitmap(bitmap);
+
+        networkImage.setImageBitmap(bitmap);
     }
 }
