8a3ed650486bd95a1cfdad8cf727d0acb4d17ec5
Basil Dalié <dalie.basil@gmail.com>
2020-05-10 21:34:30 +0200

    Implemented subscription mechanism for networks

--- app/src/main/java/com/smart/smartcity/adapters/NetworkListAdapter.java
+++ app/src/main/java/com/smart/smartcity/adapters/NetworkListAdapter.java
@@ -14,23 +14,30 @@ import android.widget.TextView;
 import com.google.android.material.textview.MaterialTextView;
 import com.smart.smartcity.R;
 import com.smart.smartcity.context.IMainFragmentContext;
+import com.smart.smartcity.context.ISubscribeContext;
+import com.smart.smartcity.dao.NetworkDAO;
 import com.smart.smartcity.model.Network;
 import com.smart.smartcity.model.Service;
+import com.smart.smartcity.model.Subscription;
+import com.smart.smartcity.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class NetworkListAdapter extends ArrayAdapter<Network> implements View.OnClickListener {
+public class NetworkListAdapter extends ArrayAdapter<Network> implements View.OnClickListener, ISubscribeContext {
     private LayoutInflater inflater;
     private IMainFragmentContext mainFragmentContext;
+    private User user;
+    private Button pressedButton = null;
 
-    public NetworkListAdapter(Context context, List<Network> data, IMainFragmentContext mainFragmentContext) {
+    public NetworkListAdapter(Context context, List<Network> data, IMainFragmentContext mainFragmentContext, User user) {
         super(context, 0, data);
 
         inflater = LayoutInflater.from(context);
         this.mainFragmentContext = mainFragmentContext;
+        this.user = user;
     }
 
     @Override
@@ -57,24 +64,77 @@ public class NetworkListAdapter extends ArrayAdapter<Network> implements View.On
 
         viewHolder.networkName.setText(network.getName());
         viewHolder.networkDescription.setText(network.getDescription());
-        viewHolder.subscribeButton.setTag(network.getId());
-        viewHolder.nameDescriptionLayout.setTag(network.getId());
+        viewHolder.subscribeButton.setTag(position);
+        viewHolder.nameDescriptionLayout.setTag(position);
         viewHolder.nameDescriptionLayout.setOnClickListener(this);
 
         if (network.getImageBitmap() != null) {
             viewHolder.networkImage.setImageBitmap(network.getImageBitmap());
         }
 
+        boolean pending = false;
+        boolean accepted = false;
+
+        for (Subscription subscription : network.getSubscriptions()) {
+            if (subscription.getUserId() == user.getId()) {
+                System.out.println("sub : " + subscription.getId() + " = " + subscription.getState());
+                if (subscription.getState().equals("pending")) {
+                    pending = true;
+                } else if (subscription.getState().equals("accepted")) {
+                    accepted = true;
+                }
+            }
+        }
+
+        if (pending) {
+            viewHolder.subscribeButton.setText("Pending ...");
+            viewHolder.subscribeButton.setEnabled(false);
+        } else if (accepted || network.getAuthorId() == user.getId()) {
+            viewHolder.subscribeButton.setVisibility(View.INVISIBLE);
+        }
+
+        viewHolder.subscribeButton.setTag(position);
+        viewHolder.subscribeButton.setOnClickListener(this);
+
         return view;
     }
 
     @Override
     public void onClick(View v) {
+        if (pressedButton != null) {
+            return;
+        }
+
         if (v.getId() == R.id.network_name_description_layout) {
             Network network = getItem((Integer) v.getTag());
 
             mainFragmentContext.showNetworkDetailsFragment(network);
+        } else if (v.getId() == R.id.network_subscribe_button) {
+            Network network = getItem((Integer) v.getTag());
+            NetworkDAO dao = new NetworkDAO();
+            dao.setSubscribeContext(this);
+            pressedButton = (Button) v;
+            v.setEnabled(false);
+            Subscription subscription = new Subscription(user.getId(), network.getId(), "pending");
+            dao.insertSubscription(subscription);
         }
+    }
+
+    @Override
+    public void onSubscribeSuccessful(Subscription subscription) {
+        Network network = getItem((Integer) pressedButton.getTag());
+
+        network.getSubscriptions().add(subscription);
+        notifyDataSetChanged();
+
+        pressedButton.setEnabled(true);
+        pressedButton = null;
+    }
+
+    @Override
+    public void onSubscribeFailure() {
+        pressedButton.setEnabled(true);
+        pressedButton = null;
     }
 
     public class ViewHolder {
