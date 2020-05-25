99cbc016be1a36f091a53b9f101a9b0eb4eba65e
Basil Dalié <dalie.basil@gmail.com>
2020-05-25 23:57:16 +0200

    Implemented trade directory and trade details views

---
+++ app/src/main/java/com/smart/smartcity/model/Trade.java
@@ -0,0 +1,78 @@
+package com.smart.smartcity.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Trade {
+    @Expose
+    @SerializedName("id")
+    private int id;
+
+    @Expose
+    @SerializedName("name")
+    private String name;
+
+    @Expose
+    @SerializedName("description")
+    private String description;
+
+    @Expose
+    @SerializedName("imageUrl")
+    private String imageUrl;
+
+    @Expose
+    @SerializedName("address")
+    private String address;
+
+    @Expose
+    @SerializedName("town")
+    private String town;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
